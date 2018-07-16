fe8573b8949b98c4ac4372f392279e996beea29d
krzemek <krzemekpl@gmail.com>
2018-07-16 09:21:23 +0200

    Inna galaz

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 
 # Zadania do wykonania test.
+# Zmiana 1.06
+# branch test
 
 import os
+
 
 loadavg5 = os.getloadavg()[1]
 print("hello")
 print("Sredni loadavg z 5 min: " + str(loadavg5))
-# Zmiana 1.06
